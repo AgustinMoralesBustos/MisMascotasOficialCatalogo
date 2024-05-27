8c1f810cb3759d93f5a4722c0a548da36bfb79fb
AgustinMoralesBustos <agusmorale77@gmail.com>
2024-05-27 17:37:35 -0300

    mantenimiento 27/05

--- MisMascotasOficial/vite.config.js
+++ MisMascotasOficial/vite.config.js
@@ -1,25 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    port: 3000,
-  },
-});
+  plugins: [react()]
+})
