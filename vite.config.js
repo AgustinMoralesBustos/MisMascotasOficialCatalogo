6c9f6d2ab8b76dfb7b9979a78b0842f4e5745699
AgustinMoralesBustos <agusmorale77@gmail.com>
2024-06-01 18:19:28 -0300

    mantenimiento 01/06

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+});
