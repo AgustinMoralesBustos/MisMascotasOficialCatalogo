06b124d82a4525a819939a03979b32014ac72a35
AgustinMoralesBustos <agusmorale77@gmail.com>
2024-06-02 13:40:35 -0300

    mantenimiento 02/06

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,10 @@ export default function NavBar() {
         setShowMenu(!showMenu);
     };
 
+    const closeMenu = () => {
+        setShowMenu(false);
+    }
+
     return (
         <nav className="menu ">
             <section className='menu-container'>
@@ -20,60 +24,60 @@ export default function NavBar() {
                 </div>
                 <ul className={`menu-links ${showMenu ? 'show' : ''} `}>
                     <li className='menu-item menu-item-show'>
-                        <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares">Nidos<img src="/arrow.png" className='menu-arrow' /></NavLink>
+                        <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares" onClick={closeMenu}>Nidos<img src="/arrow.png" className='menu-arrow' /></NavLink>
                         <ul className='menu-nesting'>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Tela">Tela</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Tela" onClick={closeMenu}>Tela</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Lona">Lona</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Lona" onClick={closeMenu}>Lona</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Cuerina">Cuerina</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/NidosRectangulares Cuerina" onClick={closeMenu}>Cuerina</NavLink>
                             </li>
                         </ul>
                     </li>
                     <li className='menu-item menu-item-show'>
-                        <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos">Moises redondos<img src="/arrow.png" className='menu-arrow' /></NavLink>
+                        <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos" onClick={closeMenu}>Moises redondos<img src="/arrow.png" className='menu-arrow' /></NavLink>
                         <ul className='menu-nesting'>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Tela">Tela</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Tela" onClick={closeMenu}>Tela</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Lona">Lona</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Lona" onClick={closeMenu}>Lona</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Cuerina">Cuerina</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesRedondos Cuerina" onClick={closeMenu}>Cuerina</NavLink>
                             </li>
                         </ul>
                     </li>
                     <li className='menu-item menu-item-show'>
-                        <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados">Moises ovalados<img src="/arrow.png" className='menu-arrow' /></NavLink>
+                        <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados" onClick={closeMenu}>Moises ovalados<img src="/arrow.png" className='menu-arrow' /></NavLink>
                         <ul className='menu-nesting'>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Tela">Tela</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Tela" onClick={closeMenu}>Tela</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Lona">Lona</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Lona" onClick={closeMenu}>Lona</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Cuerina">Cuerina</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/MoisesOvalados Cuerina" onClick={closeMenu}>Cuerina</NavLink>
                             </li>
                         </ul>
                     </li>
                     <li className='menu-item'>
-                        <NavLink className="menu-link menu-link-inside" to="/category/Blandys">Blandys<img src="/arrow.png" className='menu-arrow' /></NavLink>
+                        <NavLink className="menu-link menu-link-inside" to="/category/Blandys" onClick={closeMenu}>Blandys<img src="/arrow.png" className='menu-arrow' /></NavLink>
                         <ul className='menu-nesting'>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/Blandys Tela">Tela</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/Blandys Tela" onClick={closeMenu}>Tela</NavLink>
                             </li>
                             <li className='menu-inside'>
-                                <NavLink className="menu-link menu-link-inside" to="/category/Blandys Lona">Lona</NavLink>
+                                <NavLink className="menu-link menu-link-inside" to="/category/Blandys Lona" onClick={closeMenu}>Lona</NavLink>
                             </li>
                         </ul>
                     </li>
-                    <li className='menu-item menu-item-show'><NavLink className="menu-link menu-link-inside" to="/category/Bolsos">Bolsos</NavLink></li>
-                    <li className='menu-item menu-item-show'><NavLink className="menu-link menu-link-inside" to="/category/Especiales">Especiales</NavLink></li>
+                    <li className='menu-item menu-item-show'><NavLink className="menu-link menu-link-inside" to="/category/Bolsos" onClick={closeMenu}>Bolsos</NavLink></li>
+                    <li className='menu-item menu-item-show'><NavLink className="menu-link menu-link-inside" to="/category/Especiales" onClick={closeMenu}>Especiales</NavLink></li>
                 </ul>
                 <div className="menu-hamburger" onClick={toggleMenu}>
                     <img src="/menu.png" alt="Menu" className="menu-img" />
